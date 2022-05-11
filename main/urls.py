e03889a6618531f0b34739dc5cb65f42bb56035f
AlexGol005 <pesok1985>
2022-05-11 17:25:39 +0300

    1

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('hello/', views.TextHelloView.as_view()),
+    path('hello/', views.TextHelloView.as_view(), name='hello'),
     path('', views.IndexView.as_view()),
+    path('attestationJ/', views.AttestationJView.as_view(), name='att'),
+    path('productionJ/', views.ProductionJView.as_view(), name='prod')
 
 ]
--- viscosimeters/models.py
+++ viscosimeters/models.py
@@ -70,6 +70,26 @@ class Kalibration(models.Model):
         verbose_name = 'Калибровка'
         verbose_name_plural = 'Калибровки'
 
+#         как делать поле с выбором значений  - пример
+# class Comment(models.Model):
+#     """ Комментарии и оценки к статьям """
+#     class Ratings(models.IntegerChoices):  # https://docs.djangoproject.com/en/4.0/ref/models/fields/#enumeration-types
+#         WITHOUT_RATING = 0, _('Без оценки')
+#         TERRIBLE = 1, _('Ужасно')
+#         BADLY = 2, _('Плохо')
+#         FINE = 3, _('Нормально')
+#         GOOD = 4, _('Хорошо')
+#         EXCELLENT = 5, _('Отлично')
+#
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     # https://docs.djangoproject.com/en/4.0/topics/db/examples/many_to_one/#many-to-one-relationships
+#     note = models.ForeignKey(Note, on_delete=models.CASCADE)  # todo related_name='comments'
+#     rating = models.IntegerField(default=Ratings.WITHOUT_RATING, choices=Ratings.choices, verbose_name='Оценка')
+#
+#     def __str__(self):
+#         # https://django.fun/docs/django/ru/3.1/ref/models/instances/#django.db.models.Model.get_FOO_display
+#         return f'{self.get_rating_display()}: {self.author}'
+
 
 
 
