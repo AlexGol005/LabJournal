09a332065e798d7a6e91256218d13809a0705970
AlexGol005 <pesok1985>
2022-05-11 18:30:53 +0300

    3

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view()),
+    path('login2/', views.LoginView.as_view()),
 ]
--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from  . import views
 
 urlpatterns = [
     path('hello/', views.TextHelloView.as_view(), name='hello'),
-    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name='home'),
     path('attestationJ/', views.AttestationJView.as_view(), name='att'),
     path('productionJ/', views.ProductionJView.as_view(), name='prod')
 
