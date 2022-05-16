67c9ae7a79e50d15d6b0a0c6835d14fb453388df
AlexGol005 <pesok1985>
2022-05-16 14:40:07 +0300

    готово: пароль на весь сайт

--- viscosityattestation/urls.py
+++ viscosityattestation/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('viscosityattestationJL/<int:pk>', views.ViscosityMJLView.as_view(), name='vg'),
+    path('KVGregister/', views.ViscosityMJLCreation, name='KVGreg'),
 ]
-
