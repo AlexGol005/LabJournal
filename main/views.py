1f88c65677ba8dfafd9d19d461ace5d70513c3b1
AlexGol005 <pesok1985>
2022-05-19 14:02:21 +0300

    1

--- main/views.py
+++ main/views.py
@@ -6,35 +6,27 @@ from django.shortcuts import get_object_or_404
 from .models import AttestationJ
 
 class TextHelloView(View):
+    #просто страница для примера, адрес hello
     def get(self, request: HttpRequest) -> HttpResponse:
         text = '<h1>Hello, World</h1>'
-
         return HttpResponse(text)
 
 class IndexView(View):
+    # главная страница по основному адресу
    def get(self, request):
        return render(request, 'main/main.html')
 
 # @login_required
 class AttestationJView(View):
+    # страница Журналы аттестации
    def get(self, request):
        AttestationJObjects = AttestationJ.objects.all()
        return render(request, 'main/attestationJ.html', {'AttestationJObjects': AttestationJObjects})
 
 
 class ProductionJView(View):
+    # страница Журналы приготовления
    def get(self, request):
        return render(request, 'main/productionJ.html')
 
-# class AttestationJoneView(View):
-#     """ Представление, которое позволяет вывести отдельную запись. """
-#     def get(self, request):
-#         note = AttestationJ.objects.all()
-#         return render(request, 'main/head.html', {'note': note})
-#
-# class AttestationJoneView2(View):
-#     """ Представление, которое позволяет вывести отдельную запись. """
-#     def get(self, request):
-#         note = AttestationJ.objects.all()
-#         return render(request, 'main/head.html', {'note': note})
 
