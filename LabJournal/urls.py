ca896649488e9c04f261910ed50f667ca541c467
AlexGol005 <pesok1985>
2022-05-19 14:13:46 +0300

    1

--- LabJournal/urls.py
+++ LabJournal/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('', include('login.urls')),
     path('', include('viscosimeters.urls')),
     path('', include('kinematicviscosity.urls')),
     path('', include('users.urls')),
