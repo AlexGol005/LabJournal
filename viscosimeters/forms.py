bbb2aaa28b54911f1619b0e7207918cdd542fc29
AlexGol005 <pesok1985>
2022-05-23 18:34:06 +0300

    1

--- viscosimeters/forms.py
+++ viscosimeters/forms.py
@@ -6,7 +6,11 @@ from datetime import datetime
 from django.utils.html import format_html
 from .models import Viscosimeters, Status, Manufacturer, ViscosimeterType
 
-
+DOY = ('1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987',
+       '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995',
+       '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003',
+       '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011',
+       '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019')
 
 
 
@@ -21,9 +25,9 @@ class ViscosimetersCreationForm(forms.ModelForm):
                                                             ))
     diameter = forms.ModelChoiceField(label='Диаметр', queryset=ViscosimeterType.objects.all())
     # viscosity1000 = forms.ModelChoiceField(ViscosimeterType.objects.all(), label='вязкость/1000')
-    dateCal = forms.DateField(label='Дата калибровки',  widget=forms.SelectDateWidget())
-    datePov = forms.DateField(label='Дата поверки',  widget=forms.SelectDateWidget())
-    datePovDedlain = forms.DateField(label='Дата окончания поверки', widget=forms.SelectDateWidget())
+    dateCal = forms.DateField(label='Дата калибровки',  widget=forms.SelectDateWidget(years=DOY))
+    datePov = forms.DateField(label='Дата поверки',  widget=forms.SelectDateWidget(years=DOY))
+    datePovDedlain = forms.DateField(label='Дата окончания поверки', widget=forms.SelectDateWidget(years=DOY))
     companyName = forms.ModelChoiceField(label='Производитель', queryset=Manufacturer.objects.all())
     # range = forms.ModelChoiceField(ViscosimeterType.objects.all(),  label='Область измерений, сСт')
     status = forms.ModelChoiceField(Status.objects.all(), label='Статус')
