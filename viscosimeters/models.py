1a210bf4dd51e1d189624452624babb522169f10
AlexGol005 <pesok1985>
2022-05-30 17:58:37 +0300

    1

--- viscosimeters/models.py
+++ viscosimeters/models.py
@@ -34,12 +34,12 @@ class ViscosimeterType(models.Model):
 class Viscosimeters(models.Model):
     viscosimeterType = models.ForeignKey(ViscosimeterType,  verbose_name='Диаметр',
                                  on_delete=models.PROTECT)
-    # equipmentSM = models.ForeignKey(MeasurEquipment, verbose_name='СИ',
-    #                                      on_delete=models.PROTECT, related_name='equipmentSM', blank=True, null=True)
+    equipmentSM = models.ForeignKey(MeasurEquipment, verbose_name='СИ',
+                                         on_delete=models.PROTECT, related_name='equipmentSM', blank=True, null=True)
 
 
-    # def __str__(self):
-    #     return f'№ {self.equipmentSM.equipment.lot}'
+    def __str__(self):
+        return f'№ {self.equipmentSM.equipment.lot}'
 
     def get_absolute_url(self):
         """ Создание юрл объекта для перенаправления из вьюшки создания объекта на страничку с созданным объектом """
