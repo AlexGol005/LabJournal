1e095ddc79f0296d924e313203150938c05de8ff
AlexGol005 <pesok1985>
2022-05-30 16:33:35 +0300

    1

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Manufacturer, Rooms, \
-    Equipment, Roomschange, Personchange, MeasurEquipmentCharakters, MeasurEquipment
+from .models import Manufacturer, Rooms
+    # Equipment, Roomschange, Personchange, MeasurEquipmentCharakters, MeasurEquipment
 
 
 
 admin.site.register(Manufacturer)
-# admin.site.register(Rooms)
+admin.site.register(Rooms)
 # admin.site.register(Equipment)
 # admin.site.register(Roomschange)
 # admin.site.register(Personchange)
