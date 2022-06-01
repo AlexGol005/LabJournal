ba36c7a820fb08a4e9633130c40c88e77c0f8ce7
AlexGol005 <pesok1985>
2022-06-01 19:25:26 +0300

    1

---
+++ journalcertvalues/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import*
+
+
+admin.site.register(VG)
+admin.site.register(CharacterVG)
+admin.site.register(LotVG)
+admin.site.register(KinematicviscosityVG)
