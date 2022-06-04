0f566f00453f09baf8b20772f3966815d56b85a4
AlexGol005 <pesok1985>
2022-06-04 21:02:17 +0300

    1

--- kinematicviscosity/models.py
+++ kinematicviscosity/models.py
@@ -74,16 +74,13 @@ class ViscosityMJL(models.Model):
     fixation = models.BooleanField(verbose_name='Внесен ли результат в Журнал аттестованных значений?', default=False, null=True)
 
     def save(self, *args, **kwargs):
-        get_id_actualconstant = Kalibration.objects.filter(dateKalib__lt=self.date).select_related('id_Viscosimeter').values(
-            'id_Viscosimeter').annotate(id_actualkonstant=Max('id')).values('id_actualkonstant')
+        get_id_actualconstant = Kalibration.objects.select_related('id_Viscosimeter').\
+            filter(dateKalib__lte=self.date).filter(id_Viscosimeter__exact=self.ViscosimeterNumber1).\
+            values('id_Viscosimeter').annotate(id_actualkonstant=Max('id')).values('id_actualkonstant')
         list_ = list(get_id_actualconstant)
-        set = []
-        for n in list_:
-            set.append(n.get('id_actualkonstant'))
-        viscosimeters = Kalibration.objects.select_related('id_Viscosimeter').filter(id__in=set).filter(
-            id_Viscosimeter__equipmentSM__equipment__status__exact='Э').order_by(
-            'id_Viscosimeter__viscosimeterType__diameter')
-        self.Konstant1 = self.ViscosimeterNumber1
+        set = list_[0].get('id_actualkonstant')
+        aktualKalibration = Kalibration.objects.get(id=set)
+        self.Konstant1 = aktualKalibration.konstant
         if (self.plustimeminK1T2 and self.plustimesekK1T2 and self.plustimeminK2T1 and
                 self.plustimesekK2T1 and self.plustimeminK2T2 and self.plustimesekK2T2 and self.plustimeminK1T1 and
                 self.plustimesekK1T1):
