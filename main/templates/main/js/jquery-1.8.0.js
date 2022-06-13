a482e23b37592859900b1fc9758f385dbf1d507d
AlexGol005 <pesok1985>
2022-06-13 18:47:14 +0300

    1

--- main/templates/main/js/jquery-1.8.0.js
+++ main/templates/main/js/jquery-1.8.0.js
@@ -1,3 +1,30 @@
 $(document).ready( function() {
 $('.dropdown-toggle').dropdown();
 });
+(function(window, $){
+    var $window = $(window),
+        isWindowLoad = false;
+    $window.on('load', function(){
+        isWindowLoad = true;
+    });
+    function log(){
+        console.log.apply(console, arguments);
+    }
+    $(document).ready(function(){
+        function initMenu(){
+            var $hNav = $('#stick_menu'); /*ID фиксируемого элемента*/
+            function checkScroll(e, init){
+                init = init || false;
+                if ($window.scrollTop() > 120) { /*Высота срабатывания*/
+                    if ($('#navbar').is(':hidden')) { /*Триггер переключения*/
+                    $hNav.addClass('fixed-top');}
+                } else {
+                    $hNav.removeClass('fixed-top');
+                }
+            }
+            checkScroll(null, true);
+            $window.on('scroll.checkScroll', checkScroll);
+        }
+       initMenu();
+    });
+}(window, window.jQuery));
