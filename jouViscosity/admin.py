10fac0d2ccccaafc584e78a035a9c84ec6fc0ea7
AlexGol005 <pesok1985>
2022-06-17 16:12:10 +0300

    1

--- jouViscosity/admin.py
+++ jouViscosity/admin.py
@@ -6,7 +6,11 @@ admin.site.register(VG)
 admin.site.register(CharacterVG)
 
 admin.site.register(VGrange)
-admin.site.register(CvKinematicviscosityVG)
+
 @admin.register(LotVG)  # связываем админку с моделью
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+@admin.register(CvKinematicviscosityVG)  # связываем админку с моделью
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('id',)
