324b5186e772782fb5aaa1d2ddc894714555f0de
AlexGol005 <pesok1985>
2022-06-17 16:37:51 +0300

    1

--- users/signals.py
+++ users/signals.py
@@ -19,7 +19,7 @@ def save_profile(sender, instance, **kwargs):
 
 @receiver(post_save, sender=ViscosityMJL)
 def create_viscosityMJL(sender, instance, created, **kwargs):
-    instance = CvKinematicviscosityVG.objects.get(namelot=26)
+    instance = CvKinematicviscosityVG.objects.get(namelot=ViscosityMJL.for_lot_and_name.pk)
     if ViscosityMJL.fixation:
         if created:
             instance.cvt20 = ViscosityMJL.certifiedValue_text
