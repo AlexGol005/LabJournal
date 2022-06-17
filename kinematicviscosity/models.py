09c1cf6b63be8246819aadc94931aab95382a262
AlexGol005 <pesok1985>
2022-06-17 14:18:16 +0300

    1

--- kinematicviscosity/models.py
+++ kinematicviscosity/models.py
@@ -170,7 +170,7 @@ class ViscosityMJL(models.Model):
     # вносим АЗ в ЖАЗ
         if self.name[0:2] == 'ВЖ' and self.fixation:
             pk = CvKinematicviscosityVG.objects.get_or_create(namelot=self.for_lot_and_name)
-            note = CvKinematicviscosityVG.objects.get(pk=pk)
+            note = CvKinematicviscosityVG.objects.get(pk=pk).pk
             if self.temperature == 20.00:
                 note.cvt20 = self.certifiedValue_text
                 note.cvt20date = self.date
