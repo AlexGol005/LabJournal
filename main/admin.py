9f9c7f4b5d2e397dce1a32c441b805cbac0e3f1b
AlexGol005 <pesok1985>
2022-06-19 23:05:03 +0300

    1

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from .models import AttestationJ
+from .models import *
 
 
-@admin.register(AttestationJ)  # связываем админку с моделью
-class AttestationJAdmin(admin.ModelAdmin):
-
-    def test(self, obj: AttestationJ) -> str:
-        return f"{obj.name}"
-#
-
+admin.site.register(AttestationJ)
+admin.site.register(ProductionJ)
+admin.site.register(CertifiedValueJ)
--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 
 class AttestationJ(models.Model):
-    date = models.DateField('Дата cсоздания журнала', default=timezone.now)
+    date = models.DateField('Дата создания журнала', default=timezone.now)
     name = models.CharField('Наименование журнала', max_length=100, default='')
     ndocument = models.CharField('Методы испытаний', max_length=100, default='')
     performer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='ответственный за ведение журнала')
@@ -29,4 +29,48 @@ class AttestationJ(models.Model):
     class Meta:
         verbose_name = 'Журнал аттестации'
         verbose_name_plural = 'Журналы аттестации'
+
+class ProductionJ(models.Model):
+    date = models.DateField('Дата создания журнала', default=timezone.now)
+    name = models.CharField('Наименование журнала', max_length=100, default='')
+    ndocument = models.CharField('Методики приготовления', max_length=100, default='')
+    performer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='ответственный за ведение журнала')
+    for_url = models.CharField('Адрес журнала', max_length=100, default='')  # todo URLField
+    CM = models.TextField('Изготавливаемые СО', blank=True, null=True)
+    extra_info = models.TextField('Доп', blank=True, null=True)
+
+
+    def __str__(self):
+        return f' {self.name}'
+
+
+    def get_absolute_url(self):
+        """ Создание юрл объекта для перенаправления из вьюшки создания объекта """
+        return reverse(self.for_url)
+
+
+    class Meta:
+        verbose_name = 'Журнал приготовления'
+        verbose_name_plural = 'Журналы приготовления'
+
+class CertifiedValueJ(models.Model):
+    date = models.DateField('Дата создания журнала', default=timezone.now)
+    name = models.CharField('Наименование журнала', max_length=100, default='')
+    for_url = models.CharField('Адрес журнала', max_length=100, default='')  # todo URLField
+    CM = models.TextField('для СО', blank=True, null=True)
+    extra_info = models.TextField('Доп', blank=True, null=True)
+
+
+    def __str__(self):
+        return f' {self.name}'
+
+
+    def get_absolute_url(self):
+        """ Создание юрл объекта для перенаправления из вьюшки создания объекта """
+        return reverse(self.for_url)
+
+
+    class Meta:
+        verbose_name = 'Журнал аттестованных значений'
+        verbose_name_plural = 'Журналы аттестованных значений'
 
