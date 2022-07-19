27b44816e8669ded43d9b3b4e2f775a4fd0ac274
AlexGol005 <pesok1985>
2022-07-19 16:32:27 +0300

    2

--- viscosimeters/views.py
+++ viscosimeters/views.py
@@ -75,28 +75,28 @@ class ViscosimetersHeadView(View):
 
 
 
-# -------------------
-import xlwt
-
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-
-def export_users_xls(request):
-    response = HttpResponse(content_type='application/ms-excel')
-    response['Content-Disposition'] = 'attachment; filename="users.xls"'
-
-    wb = xlwt.Workbook(encoding='utf-8')
-    ws = wb.add_sheet('Users')
-
-    # Sheet header, first row
-    row_num = 3
-
-    font_style = xlwt.XFStyle()
-    font_style.font.bold = True
-
-    columns = ['Имя', 'First name', 'Last name', 'Email address', ]
-
-    for col_num in range(len(columns)):
+# # -------------------
+# import xlwt
+#
+# from django.http import HttpResponse
+# from django.contrib.auth.models import User
+#
+# def export_users_xls(request):
+#     response = HttpResponse(content_type='application/ms-excel')
+#     response['Content-Disposition'] = 'attachment; filename="users.xls"'
+#
+#     wb = xlwt.Workbook(encoding='utf-8')
+#     ws = wb.add_sheet('Users')
+#
+#     # Sheet header, first row
+#     row_num = 3
+#
+#     font_style = xlwt.XFStyle()
+#     font_style.font.bold = True
+#
+#     columns = ['Имя', 'First name', 'Last name', 'Email address', ]
+#
+#     for col_num in range(len(columns)):
         ws.write(row_num, col_num, columns[col_num], font_style)
 
     # Sheet body, remaining rows
