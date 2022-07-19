de8301e82420aeb5ac88935adb1d32d0d8646635
AlexGol005 <pesok1985>
2022-07-19 16:35:18 +0300

    2

--- viscosimeters/urls.py
+++ viscosimeters/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('viscosimeterskonstants/', views.ViscosimetersView.as_view(), name='konstv'),
     path('kalibrationviscosimetersreg/', views.KalibrationViscosimetersRegView, name='kalibrationviscosimetersreg'),
     path('viscosimeters/', views.ViscosimetersHeadView.as_view(), name='vt'),
-    path(r'^export/xls/$', views.export_users_xls, name='export_users_xls'),
+    # path(r'^export/xls/$', views.export_users_xls, name='export_users_xls'),
 ]
 
 
