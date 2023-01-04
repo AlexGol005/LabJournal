28a70f55346f11e5aad43b05bed71c6a299d6a04
AlexGol005 <pesok1985>
2023-01-04 12:56:39 +0300

    1

--- equipment/views.py
+++ equipment/views.py
@@ -605,7 +605,6 @@ class HaveorderVerView(UpdateView):
             get(equipmentSM_id=self.kwargs['pk'])
         return q
 
-
     def get_context_data(self, **kwargs):
         context = super(HaveorderVerView, self).get_context_data(**kwargs)
         context['title'] = "Заказана поверка или новое СИ"
