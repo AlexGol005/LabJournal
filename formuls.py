ad9b4981a0c216a804a0dfff2aa186e6e1100d41
AlexGol005 <pesok1985>
2023-11-12 17:59:22 +0300

    12/11 (округление больших чисел)

--- formuls.py
+++ formuls.py
@@ -62,6 +62,12 @@ def  numberDigits(avg: Decimal, abserror: Decimal) -> Decimal:
         if abserror > 29:
             abserror = str(abserror)
             if int(abserror[0]) <= K:
+                certifiedValue = (str(certifiedValue)[: -(len(abserror) - 2)]) + '.' + str(certifiedValue)[-(len(abserror) - 2):]
+                certifiedValue = Decimal(certifiedValue).quantize(Decimal('1'), ROUND_HALF_UP)
+                tail = (len(abserror) - 2) * '0'
+                certifiedValue = str(certifiedValue) + tail
+                return Decimal(certifiedValue)
+            if int(abserror[0]) > K:
                 certifiedValue = (str(certifiedValue)[: -(len(abserror) + 2)]) + '.' + str(certifiedValue)[-(len(abserror) + 2):]
                 certifiedValue = Decimal(certifiedValue).quantize(Decimal('1'), ROUND_HALF_UP)
                 tail = (len(abserror) - 2) * '0'
