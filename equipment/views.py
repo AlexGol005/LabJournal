6d2c7581bf8c2f561b8b831ad4d842dc26870c60
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2023-12-11 14:27:20 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -4614,10 +4614,11 @@ def export_metroyearcust_xls(request):
         annotate(mod_type=Concat('charakters__typename', Value(' '), 'charakters__modificname'),
                  manuf_country=Concat('equipment__manufacturer__country', Value(', '),
                                       'equipment__manufacturer__companyName')). \
-        filter(equipment__personchange__in=setperson). \
         filter(equipment__roomschange__in=setroom). \
+        filter(equipment__personchange__in=setperson). \
         filter(equipmentSM_att__in=setatt). \
         filter(equipmentSM_att__date__year=serdate). \
+        filter(equipmentSM_att__price__isnull=False). \
         exclude(equipmentSM_att__cust=True). \
         values_list(
         'equipment__exnumber',
@@ -4626,7 +4627,7 @@ def export_metroyearcust_xls(request):
         'equipment__lot',
         'equipmentSM_att__certnumber',
         'equipmentSM_att__date',
-        'equipmentSM_att__datedead'
+        'equipmentSM_att__datedead',
         'equipmentSM_att__price',
     ).order_by('equipmentSM_att__date')
 
