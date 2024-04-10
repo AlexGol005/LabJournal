d23eaa38048c746663a8cd438b11699ee255daba
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-04-10 13:44:09 +0300

    Update views.py

--- kinematicviscosity/views.py
+++ kinematicviscosity/views.py
@@ -1402,7 +1402,7 @@ def export_protocol_xls(request, pk):
         for row in qseria:
             row_num += 1
             for col_num in range(1, 6):
-                ws.write(row_num, col_num + 1, row[col_num], style8)
+                ws.write(row_num, col_num, row[col_num], style8)
 
 
 
