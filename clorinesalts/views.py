67d19f5e7fbed3e34e14f37b3fdf73e4a1560211
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-04-27 11:49:52 +0300

    Update views.py

--- clorinesalts/views.py
+++ clorinesalts/views.py
@@ -610,14 +610,14 @@ def export_protocol_xls(request, pk):
 
 
     for i in range(len(MATERIAL)):
-        if self.name == MATERIAL[i][0]:
-            self.constit = constitoptional[i]
+        if note.name == MATERIAL[i][0]:
+            note.constit = constitoptional[i]
 
     ndocument = note.ndocument
 
     for i in range(len(aimoptional)):
-        if self.aim == aimoptional[i][0]:
-            self.constit = conclusionoptional[i]
+        if note.aim == aimoptional[i][0]:
+            note.constit = conclusionoptional[i]
     
     response = HttpResponse(content_type='application/ms-excel')
     response['Content-Disposition'] = f'attachment; filename="{note.pk}_protocol.xls"'
