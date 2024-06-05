8d58e14a9c611ab0d675fa3aea4f018efcc9267f
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-06-05 14:36:14 +0300

    Update views.py

--- kinematicviscosity/views.py
+++ kinematicviscosity/views.py
@@ -36,8 +36,8 @@ class Constants:
     SearchDateForm = SearchDateForm
 # конец блока для всех журналов
 
-def export_protocol_xls_template_1(request, pk):
-    response = export_protocol_xls_template(MATERIAL, MODEL, constitoptional, aimoptional, conclusionoptional, attcharacteristic)
+def export_protocol_xls_template_1(request):
+    response = export_protocol_xls_template(pk, MATERIAL, MODEL, constitoptional, aimoptional, conclusionoptional, attcharacteristic)
     return response
 
 def SeriaUpdate(request, str):
