c2154adb7643c1f26027541fa19e1c0b01ece82c
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-06-05 15:11:12 +0300

    Update views.py

--- kinematicviscosity/views.py
+++ kinematicviscosity/views.py
@@ -37,8 +37,7 @@ class Constants:
 # конец блока для всех журналов
 
 def export_protocol_xls_template_1(request, pk):
-    pk=pk
-    response = export_protocol_xls_template(1876, MATERIAL, MODEL, constitoptional, aimoptional, conclusionoptional, attcharacteristic)
+    response = export_protocol_xls_template(pk, MATERIAL, MODEL, constitoptional, aimoptional, conclusionoptional, attcharacteristic)
     return response
 
 def SeriaUpdate(request, str):
