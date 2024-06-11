b436a7a71ca8ba93f1d8d513b01611b50a037ec7
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-06-11 16:58:46 +0300

    Update models.py

--- kinematicviscosity/models.py
+++ kinematicviscosity/models.py
@@ -110,7 +110,7 @@ class ViscosityMJL(models.Model):
     repr1 = models.CharField('Повторяемость, мм2/с', max_length=90, null=True, blank=True)
     Rep2 = models.CharField('Воспроизводимость, мм2/с', max_length=90, null=True, blank=True)
     crit_K = models.CharField('Критерий К, мг/л', max_length=90, null=True, blank=True)
-    
+    index = models.CharField('ggg', max_length=100, default='0', null=True,  blank=True)
     x1 = models.CharField('ggg', max_length=100, default='0', null=True,  blank=True)
     x2 = models.CharField('ggg', max_length=100, default='0', null=True,  blank=True)
     x_avg = models.CharField('ggg', max_length=100, default='0', null=True,  blank=True)
