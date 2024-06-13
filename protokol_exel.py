6dca4e49bfae3e42955287068f616f8a59ef422a
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-06-13 15:02:23 +0300

    Update protokol_exel.py

--- protokol_exel.py
+++ protokol_exel.py
@@ -214,8 +214,7 @@ def export_protocol_xls_template(num, MATERIAL1, MODEL, constitoptional, aimopti
     if not ser.equipment1:
         timer_forset = MeasurEquipment.objects.get(equipment__exnumber=timer)
         equipment_set7 = f'{timer_forset.charakters.name} тип {timer_forset.charakters.typename}, зав. № {timer_forset.equipment.lot}, свидетельство о поверке № {timer_forset.newcertnumber} от {timer_forset.newdate} действительно до {timer_forset.newdatedead}.'    
-    except:
-        pass
+
     #конец поиск х1 и х2 по кинематике - костыль для динамики
 
     
