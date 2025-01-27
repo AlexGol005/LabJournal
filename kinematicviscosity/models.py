383a0c1fdaf754659c4fbc7597c933f128551423
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-27 10:59:14 +0300

    Update models.py

--- kinematicviscosity/models.py
+++ kinematicviscosity/models.py
@@ -180,18 +180,17 @@ class ViscosityMJL(models.Model):
         if self.constit == 'по ГОСТ 33':
             self.kriteriy = Decimal(0.35)
         if self.constit == 'ост. топлива; мазут; при 50 °С':
-            self.kriteriy = Decimal(1.5)
+            self.kriteriy = 1.5
 
         
-        # if Decimal(self.accMeasurement).quantize(Decimal('1.00000'), ROUND_HALF_UP) <= self.kriteriy:
-        #     self.resultMeas = 'удовлетворительно'
-        #     self.cause = ''
-        # if Decimal(self.accMeasurement).quantize(Decimal('1.00000'), ROUND_HALF_UP) > self.kriteriy:
-        #     trr = (Decimal(self.accMeasurement).quantize(Decimal('1.00000'), ROUND_HALF_UP))
-        #     self.resultMeas = 'неудовлетворительно'
-        #     self.cause = ':  Δ > r'
-        self.resultMeas = 'удовлетворительно'
-        self.cause = ''
+        if Decimal(self.accMeasurement).quantize(Decimal('1.00000'), ROUND_HALF_UP) <= self.kriteriy:
+            self.resultMeas = 'удовлетворительно'
+            self.cause = ''
+        if Decimal(self.accMeasurement).quantize(Decimal('1.00000'), ROUND_HALF_UP) > self.kriteriy:
+            trr = (Decimal(self.accMeasurement).quantize(Decimal('1.00000'), ROUND_HALF_UP))
+            self.resultMeas = 'неудовлетворительно'
+            self.cause = ':  Δ > r'
+
         if self.resultMeas == 'удовлетворительно':
             self.abserror = mrerrow((Decimal(self.relerror) * self.viscosityAVG) / Decimal(100))
             self.certifiedValue = numberDigits(self.viscosityAVG, self.abserror)
