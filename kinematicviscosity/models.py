d22a3b9bb4cd364b816e7c9c20923ae3dcd07406
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-27 11:09:00 +0300

    Update models.py

--- kinematicviscosity/models.py
+++ kinematicviscosity/models.py
@@ -180,7 +180,7 @@ class ViscosityMJL(models.Model):
         if self.constit == 'по ГОСТ 33':
             self.kriteriy = Decimal(0.35)
         if self.constit == 'ост. топлива; мазут; при 50 °С':
-            pass
+            self.kriteriy = Decimal(1.5)
 
         
         # if Decimal(self.accMeasurement).quantize(Decimal('1.00000'), ROUND_HALF_UP) <= self.kriteriy:
@@ -197,8 +197,8 @@ class ViscosityMJL(models.Model):
             self.certifiedValue = numberDigits(self.viscosityAVG, self.abserror)
         self.repr1 = Decimal(self.viscosityAVG)*Decimal(self.kriteriy)/ Decimal(100)
         try: 
-            self.self.repr1
-            self.repr1 = numberDigits(self.repr1, self.abserror)
+            self.repr1
+            # self.repr1 = numberDigits(self.repr1, self.abserror)
         except:
             pass
         if self.constit == 'по ГОСТ 33' and self.resultMeas == 'удовлетворительно':
