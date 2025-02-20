869f9c5924988f7ac26655f16746df2aead1106e
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-20 10:53:27 +0300

    Update forms.py

--- kinematicviscosity/forms.py
+++ kinematicviscosity/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.db.models import  Q
+from django.db.models import Subquery
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
