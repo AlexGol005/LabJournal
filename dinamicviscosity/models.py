d6088e730e0b231bc87b1085b3e64cd3c69013c1
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-25 11:04:59 +0300

    Update models.py

--- dinamicviscosity/models.py
+++ dinamicviscosity/models.py
@@ -141,7 +141,7 @@ class Dinamicviscosity(models.Model):
                     self.kriteriy = Decimal(0.3)
                 self.accMeasurement = get_acc_measurement(self.density1, self.density2)
                 # if self.accMeasurement < self.kriteriy:
-                if self.accMeasurement =< Decimal(0.3):
+                if self.accMeasurement <= Decimal(0.3):
                     self.resultMeas = 'удовлетворительно'
                     self.cause = ''
                     self.density_avg = get_avg(self.density1, self.density2, 4)
